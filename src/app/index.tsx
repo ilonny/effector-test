3c1eed7090c32988fcfd145f46b5fa3c583103a8
Dmitry Gornak <lonnyfox@bk.ru>
2021-10-25 16:16:00 +0300

    init eslint

---
+++ src/app/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <p>
+        Edit
+        {' '}
+        <code>src/App.tsx</code>
+        {' '}
+        and save to reload.
+      </p>
+    </header>
+  </div>
+);
+
+export default App;
